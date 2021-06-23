7aa7f8396c94b8bb9bfb768aaaaf334f674bc936
Kedari Teja <kedariteja1999@gmail.com>
2021-06-23 11:46:34 +0530

    initial commit

---
+++ Spring_Data_JPA_Repository_Testing/src/main/java/com/vm/spring/SpringDataJpaRepositoryTestingApplication.java
@@ -0,0 +1,13 @@
+package com.vm.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaRepositoryTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaRepositoryTestingApplication.class, args);
+	}
+
+}
